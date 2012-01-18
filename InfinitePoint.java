a011eb98dceb9b513c27759b0f9405828dcf0576
eric.lebon.pro <eric.lebon.pro@gmail.com>
2012-01-18 09:36:43 +0000

    Methode distanceEuclidienne

--- InfinitePoint.java
+++ InfinitePoint.java
@@ -7,4 +7,18 @@ public class InfinitePoint {
 	public InfinitePoint(LinkedList<Double>variables){
 		this.variables = new LinkedList<Double>(variables);
 	}
+
+	/**
+	 * Distance euclidienne
+	 */
+	public double  distance(InfinitePoint p){
+		if (variables == null || variables.size() < 1)
+			return -1;
+
+		double distance =0;
+		for(Double d : variables){
+			distance += d*d;
+		}
+		return distance;
+	}
 }
