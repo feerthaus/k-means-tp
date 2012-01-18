59bf603b9b59c2afe6826fdb1cc726b8f6eb3bef
eric.lebon.pro@gmail.com <eric.lebon.pro@gmail.com@1fa53bb5-8a24-186a-bd39-1e9c3f0e32ec>
2012-01-18 10:15:53 +0000

    classe contenant l'algorithme de clusterisation. contient le constructeur et la méthode de réallocation

---
+++ trunk/Kmeans.java
@@ -0,0 +1,54 @@
+import java.util.LinkedList;
+
+
+public class Kmeans {
+	protected  LinkedList<InfinitePoint> data =new LinkedList<InfinitePoint>();
+	protected  LinkedList<InfinitePoint> gravityCenters = new LinkedList<InfinitePoint>();
+	LinkedList<LinkedList<InfinitePoint>>groups = null;
+	
+	
+	/**
+	 * Constructor
+	 * @param data
+	 * @param gravityCenters
+	 */
+	public Kmeans(LinkedList<InfinitePoint> data, LinkedList<InfinitePoint> gravityCenters){
+		if (data != null)
+			this.data = data;
+		
+		if (gravityCenters != null)
+		this.gravityCenters = gravityCenters;
+		
+	}
+	
+	
+	/**
+	 * reallocation step for k-means method
+	 */
+	public void reallocation(){
+		
+			groups = new LinkedList<LinkedList<InfinitePoint>>();
+			for (InfinitePoint g: gravityCenters ){
+				groups.add(new LinkedList<InfinitePoint>());
+			}
+			for (InfinitePoint p : data){
+				int closest_gravityCenterId = -1;
+				double min_distance = Double.MAX_VALUE;
+				for (int i = 0; i < gravityCenters.size(); i++){
+					InfinitePoint g = gravityCenters.get(i);
+					double current_distance = g.distanceEuclidienne(p);
+					if (current_distance< min_distance){
+						min_distance = current_distance;
+						closest_gravityCenterId = i;
+					}
+					
+					groups.get(i).add(p);
+				}
+			}
+			
+			
+		
+	}
+	
+	
+}
