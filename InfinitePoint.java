a99ec00b5c12d469b92310d62233f707c2850856
eric.lebon.pro <eric.lebon.pro@gmail.com>
2012-01-18 09:23:33 +0000

    nouvelle version du point

---
+++ InfinitePoint.java
@@ -0,0 +1,10 @@
+import java.util.LinkedList;
+
+
+public class InfinitePoint {
+	LinkedList<Double> variables = new LinkedList<Double>();
+
+	public InfinitePoint(LinkedList<Double>variables){
+		this.variables = new LinkedList<Double>(variables);
+	}
+}
