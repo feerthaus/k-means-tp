6ad1be2947460d7c1f5c6ca89c24f1f00c72e9b6
eric.lebon.pro <eric.lebon.pro@gmail.com>
2012-01-18 09:14:21 +0000

    Chaque point contenant un nombre indéfini de variables

---
+++ Point.java
@@ -0,0 +1,10 @@
+import java.util.LinkedList;
+
+
+public class Point {
+	LinkedList<Double> variables = new LinkedList<Double>();
+
+	public Point(LinkedList<Double>variables){
+		this.variables = new LinkedList<Double>(variables);
+	}
+}
