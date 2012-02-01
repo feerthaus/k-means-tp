82f328dc927f377b18a1fd2f3e6f9a856edbf174
eric.lebon.pro <eric.lebon.pro@gmail.com>
2012-02-01 08:25:25 +0000

    class Main

---
+++ kmeans/src/Main.java
@@ -0,0 +1,17 @@
+/**
+ *
+ * @author : LEBON Eric
+ * @version : 1.0
+ * @date : 1 févr. 2012 
+ */
+package src;
+
+/**
+ * Main
+ *Contains the main function
+ */
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
